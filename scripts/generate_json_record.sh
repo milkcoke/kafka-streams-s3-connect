f6f40040d30a49ac35bdbd246769cabf1315002f
milkcoke <mbh023@gmail.com>
2024-05-15 21:41:34 +0900

    feat: Add generate_json_record.sh
    - Generate the ndjson format file including S3 bucket and path

---
+++ scripts/generate_json_record.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+uploaded_list_file=$1
+s3_files=$(cat "$uploaded_list_file")
+
+for s3_file in $s3_files; do
+  bucket_name=$(echo "$s3_file" | awk -F '/' '{print $3}')
+  object_path=$(echo "$s3_file" | cut -d/ -f4- | sed 's|^|/|')
+  printf '{"bucket": "%s", "path": "%s"}\n' "$bucket_name" "$object_path" >> "records.ndjson"
+done
+
+
