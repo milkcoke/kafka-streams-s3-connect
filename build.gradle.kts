86561288d1334a03c82d03c9280942978e162315
milkcoke <mbh023@gmail.com>
2024-05-25 15:41:00 +0900

    build: Update build.gradle.kts
    - Add dependency constraints of okhttp.
    - Should use 5.0.x version for okhttp.

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,23 @@ dependencies {
 	implementation("org.apache.kafka:kafka-streams:3.7.0")
 	implementation ("io.micrometer:micrometer-registry-prometheus:1.12.5")
 
-	implementation("aws.sdk.kotlin:s3:1.2.15")
+	// https://github.com/awslabs/aws-sdk-kotlin/issues/765
+	implementation("aws.sdk.kotlin:s3:1.2.15") {
+		constraints {
+			implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.14") {
+				because("okhttp3 ~v4 does not support Request builder (kotlin reflect)")
+			}
+		}
+	}
+	configurations.all {
+		// https://docs.gradle.org/current/dsl/org.gradle.api.artifacts.ResolutionStrategy.html
+		resolutionStrategy.eachDependency {
+			if (requested.group == "com.squareup.okhttp3" && requested.name == "okhttp") {
+				useVersion("5.0.0-alpha.14")
+				because("okhttp3 ~v4 does not support Request builder (kotlin reflect) on AWS SDK")
+			}
+		}
+	}
 
 	implementation("org.slf4j:slf4j-api:2.0.12")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
