8dd7448caf9f83bdde849011d4c286ed254cbf16
milkcoke <mbh023@gmail.com>
2025-04-11 23:58:51 +0900

    build: Configure multi module projects
    - Separate kafka streams connect and s3-source-connect.
    - Kafka Streams connect is old version which has limit the s3 file size should be small.
    - S3 source connect is main project for this.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,6 @@
+plugins {
+  id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
 rootProject.name = "s3-source-connect"
+include("kafka-streams-connect")
+include("s3-source-connector")
