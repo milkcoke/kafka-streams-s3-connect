4fb6dabfbaefc5496e909c67d2cb0ec1783a8c3c
milkcoke <mbh023@gmail.com>
2025-04-15 00:19:00 +0900

    feat: Add maxPollRecords property on KafkaConfig
    - This is used for poll() for scanning the offset-topic in reverse.

--- s3-source-connector/src/main/java/sourceconnector/config/KafkaConfig.java
+++ s3-source-connector/src/main/java/sourceconnector/config/KafkaConfig.java
@@ -2,6 +2,7 @@ package sourceconnector.config;
 
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,9 @@ import java.util.Properties;
 
 @Configuration
 public class KafkaConfig {
+  @Value("${spring.kafka.consumer.max-poll-records}")
+  private int maxPollRecords;
+
   @Bean
   public Properties produerProperties(KafkaProperties kafkaProperties) {
     Properties properties = new Properties();
